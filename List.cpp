4b0efd1e52705c617c0cd051b890543f02f3e268
Fernando Cesar <cmnando.silva@gmail.com>
2020-09-15 03:13:48 -0300

    List::Last terminado

--- List.cpp
+++ List.cpp
@@ -18,14 +18,16 @@ void List::insert(int value)
 }
 int List::remove()
 {
-    if (this->isEmpty() == 0)
+    if (this->isEmpty() == 1)
     {
         return NULL;
     }
     else
     {
-        this->head = new Node(this->head->getNext()->getData(), this->head->getNext()->getNext());
+        int a = this->head->getData();
+        this->head = new Node(this->head->getNext());
         this->decreaseSize();
+        return a;
     }
 }
 int List::isEmpty()
@@ -43,11 +45,6 @@ unsigned List::size() const
 {
     return this->_size;
 }
-/** Returns the element in the middle of the list.
- If the list has 2*n or 2*n+1 elements, then the middle element is
- the element at index n, assuming that the first index is zero.
- @returns the middle element of the list.
- */
 int List::middle() const
 {
     int middle;
@@ -56,15 +53,17 @@ int List::middle() const
     else
         middle = (this->size() - 1) / 2;
     Node *aux = new Node(this->head);
-    for (int i = 0; i < this->size(); i++)
-        if (i != this->size() - 1)
+    for (int i = 0; i < middle; i++)
+        aux = new Node(aux->getNext());
+    return aux->getData();
+}
+int List::last() const
+{
+    Node *aux = new Node(this->head);
+    for (int i = 0; i < this->size()-1; i++)
             aux = new Node(aux->getNext());
     return aux->getData();
 }
-/** Returns the last element in the linked list.
- @returns the integer stored in the last element of the list.
- */
-int List::last() const {}
 /** This method moves the head of the list to its last position.
  In other words, rotate() + last() == head.
  */
--- main.cpp
+++ main.cpp
@@ -12,12 +12,22 @@ int main(int argc, char **argv)
     }
     std::cout << "s:" << list->size() << ", ";
     std::cout << "m:" << list->middle() << ", ";
-    //std::cout << "l:" << list->last() << ", ";
+    std::cout << "l:" << list->last() << ", ";
     std::cout << "r:" << list->remove() << ", ";
-    //std::cout << "m:" << list->middle() << ", ";
-    //std::cout << "l:" << list->last() << ", ";
+    std::cout << "m:" << list->middle() << ", ";
+    std::cout << "l:" << list->last() << ", ";
     //list->rotate();
     //std::cout << "m:" << list->middle() << ", ";
     //std::cout << "l:" << list->last() << std::endl;
     return 0;
 }
+/*
+2 3 5 7 11 0
+s:5, m:5, l:2, r:11, m:3, l:2, m:2, l:7
+
+2 3 5 7 11 13 17 19 23 29 31 37 0
+s:12, m:13, l:2, r:37, m:13, l:2, m:11, l:31
+
+1 2 3 4 5 6 7 8 9 0
+s:9, m:5, l:1, r:9, m:4, l:1, m:3, l:8
+*/
